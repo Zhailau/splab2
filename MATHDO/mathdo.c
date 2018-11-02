26236249276961fbf3cfbc2f5248e68ce7b4240a
Nagima Zhailau <43665029+Zhailau@users.noreply.github.com>
2018-11-02 17:24:15 +0600

    Update mathdo.c

--- MATHDO/mathdo.c
+++ MATHDO/mathdo.c
@@ -6,18 +6,35 @@
 int main(int argc, char* argv[]){
 	int pid = fork(); // child1
 	if (pid==0){ // child 1
-		execlp("./add.py",":)",argv[1],argv[2],NULL);
+		execlp("./p.py",":)",argv[1],argv[2],NULL);
 
 	} else // parent
 	{
 		int pid2 = fork();
 		if (pid2==0){ // child 2
-			execlp("./div.sh",":)",argv[1],argv[2],NULL);
+			execlp("./b.sh",":)",argv[1],argv[2],NULL);
 		} else // parent
 		{
-			wait(0);
-			wait(0);
-			printf("parent: done\n");
+			int pid3 = fork();
+			if (pid3==0){ // child 2
+				execlp("java","java","j",argv[1],argv[2],NULL);
+			} else // parent
+			{	
+				int pid4 = fork();
+				if (pid4==0){ // child 2
+					execlp("./n.js",":)",argv[1],argv[2],NULL);
+				} else // parent
+				{
+					wait(0);
+					wait(0);
+					wait(0);
+					wait(0);
+					printf("parent: done\n");
+				}
+			}
 		}		
 	}
 }
+
+
+
